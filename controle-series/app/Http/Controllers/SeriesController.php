39daf328b4da732551b3a793ebb1d25be797dbcb
Thiago Luz <thluzbs@gmail.com>
2025-04-09 12:02:57 -0300

    validação dos dados de requisição, regras de validações, extrair e exibir mensagens de erro ou sucesso

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeriesFormRequest;
 use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -20,7 +21,8 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request) {
+    public function store(SeriesFormRequest $request) {
+
         $serie = Serie::create($request->all());
         //$request->session()->flash('mensagem.sucesso', "Série {$serie->nome} adicionada com sucesso");
 
@@ -41,7 +43,7 @@ class SeriesController extends Controller
         return view('series.edit')->with('serie', $series);
     }
 
-    public function update(Serie $series, Request $request){
+    public function update(Serie $series, SeriesFormRequest $request){
         $series->fill($request->all());
         $series->save();
 
--- controle-series/resources/views/series/edit.blade.php
+++ controle-series/resources/views/series/edit.blade.php
@@ -1,3 +1,3 @@
 <x-layout title="Editar Série {{ $serie->Nome }}">
-  <x-series.form :action="route('series.update', $serie->id)" :nome="$serie->Nome"/>
+  <x-series.form :action="route('series.update', $serie->id)" :nome="$serie->Nome" :update="true"/>
 </x-layout>
