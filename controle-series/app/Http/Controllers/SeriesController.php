7da8d60e311e43529b80d214bdad7fc9092b5915
Thiago Luz <thluzbs@gmail.com>
2025-05-06 22:54:03 -0300

    Separando o código que lida com banco de dados numa classe repository

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -6,11 +6,18 @@ use App\Http\Requests\SeriesFormRequest;
 use App\Models\Episode;
 use App\Models\Season;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
+
+    public function __construct(private SeriesRepository $repository)
+    {
+        
+    }
+
     public function index(Request $request) {
         //$series = Serie::query()->orderBy('Nome')->get();
         $series = Series::all();
@@ -26,36 +33,7 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = DB::transaction(function() use ($request){
-            $serie = Series::create($request->all());
-            $seasons = [];
-            for ($i = 1; $i <= $request->seasonsQty; $i++){
-                $seasons[] = [
-                    'series_id' => $serie->id,
-                    'number' => $i
-                ];
-            }
-            Season::insert($seasons);
-    
-            $episodes = [];
-            foreach($serie->seasons as $season){
-                for ($j = 1; $j <= $request->episodesPerSeason; $j++){
-                    $episodes[] = [
-                        'season_id' => $season->id,
-                        'number' => $j
-                    ];
-                }
-            }
-            Episode::insert($episodes);
-    
-            //$request->session()->flash('mensagem.sucesso', "Série {$serie->nome} adicionada com sucesso");
-    
-            //DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie]);
-
-            return $serie;
-        });
-
-        assert($serie !== null);
+        $serie = $this->repository->add($request);
         return to_route('series.index')->with('mensagem.sucesso', "Série {$serie->nome} adicionada com sucesso");
 
     }
