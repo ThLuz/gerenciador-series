28a21af07de8a64363ec5a1b41f3485532197d12
Thiago Luz <thluzbs@gmail.com>
2025-05-16 12:03:06 -0300

    começando adicionar Mail para alertar criação de serie

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Mail\SeriesCreated;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/email', function () {
+    return new SeriesCreated('Teste', 17, 5, 10);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
